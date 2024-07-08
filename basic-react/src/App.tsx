a34e028e6ffba6ce7a7c003cd7485160cefe62c2
jungcolor <jungcolor@gmail.com>
2024-07-08 23:40:36 +0900

    state, effect hook

--- basic-react/src/App.tsx
+++ basic-react/src/App.tsx
@@ -1,13 +1,19 @@
-import { ChangeEvent, Dispatch, ReactNode, SetStateAction, useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+    // 배열 구조분해 할당을 활용
+    // useState의 인자는 초기값을 넣어줌
     const [count, setCount] = useState(0);
 
+    // useEffect가 실행되는 시점에 DOM이 업데이트되었음을 보장
     useEffect(() => {
         document.title = `You clicked ${count} times`;
 
         // effect를 해제하려면 함수를 반환
+        // 정리(clean up)가 필요한 경우 함수 반환
         return () => { }
+
+        // 정리(clean up)가 필요하지 않은 경우 아무것도 반환하지 않음
     });
 
     return (
