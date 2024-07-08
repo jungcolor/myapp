779fdc56e5169e0478851efbc7a70b36390d8d57
jungcolor <jungcolor@gmail.com>
2024-07-08 23:20:43 +0900

    add to hook summary

--- basic-react/src/App.tsx
+++ basic-react/src/App.tsx
@@ -1,7 +1,19 @@
+import { ChangeEvent, Dispatch, ReactNode, SetStateAction, useCallback, useEffect, useState } from "react";
+
 function App() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+
+        // effect를 해제하려면 함수를 반환
+        return () => { }
+    });
+
     return (
-        <div className="App">
-            가나다라
+        <div>
+            <p>you clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
         </div>
     );
 }
