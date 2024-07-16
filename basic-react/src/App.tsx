4a96e30165b78de13881a481d70edb6e47ee7b12
jungcolor <jungcolor@gmail.com>
2024-07-16 23:18:07 +0900

    not use useCallback

--- basic-react/src/App.tsx
+++ basic-react/src/App.tsx
@@ -1,31 +1,26 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
     const [number, setNumber] = useState<number | string>(0);
-    const [isKorea, setIsKorea] = useState(true);
 
-    const location = useMemo(() => {
-        return {
-            country: isKorea ? "한국" : "외국"
-        }
-    }, [isKorea]);
+    const someFuntion = () => {
+        console.log(`someFunc: number: ${number}`);
+        return;
+    };
 
     useEffect(() => {
-        console.log("useEffect 호출");
-    }, [location]);
+        console.log("someFunction이 변경되었습니다.");
+    }, [someFuntion]);
 
     return (
         <div>
-            <h2>하루에 몇끼 먹어요?</h2>
             <input
                 type="number"
                 value={number}
                 onChange={(e) => setNumber(e.target.value)}
             />
-            <hr />
-            <h2>어느 나라에 있어요?</h2>
-            <p>나라: {location.country}</p>
-            <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
+            <br />
+            <button onClick={someFuntion}>Call someFunc</button>
         </div>
     );
 }
