a28ac80e49cd779eaeeca77c5733bd0307beeb8c
jungcolor <jungcolor@gmail.com>
2024-07-16 23:05:50 +0900

    ex2 use useMemo

--- basic-react/src/App.tsx
+++ basic-react/src/App.tsx
@@ -1,44 +1,31 @@
-import "./app.css";
-import { useMemo, useState } from "react";
-
-const hardCalculate = (number: number) => {
-    console.log("어려운 계산!");
-    for (let i = 0; i < 999999999; i++) { }
-    return number + 10000;
-}
-
-const easyCalculate = (number: number) => {
-    console.log("쉬운 계산");
-    return number + 1;
-}
+import { useEffect, useMemo, useState } from "react";
 
 const App = () => {
-    const [hardNumber, setHardNumber] = useState(1);
-    const [easyNumber, setEasyNumber] = useState(1);
+    const [number, setNumber] = useState<number | string>(0);
+    const [isKorea, setIsKorea] = useState(true);
 
-    // const hardSum = hardCalculate(hardNumber);
-    const hardSum = useMemo(() => {
-        return hardCalculate(hardNumber);
-    }, [hardNumber]);
-    const easySum = easyCalculate(easyNumber);
+    const location = useMemo(() => {
+        return {
+            country: isKorea ? "한국" : "외국"
+        }
+    }, [isKorea]);
+
+    useEffect(() => {
+        console.log("useEffect 호출");
+    }, [location]);
 
     return (
         <div>
-            <h3>어려운 계산기</h3>
+            <h2>하루에 몇끼 먹어요?</h2>
             <input
                 type="number"
-                value={hardNumber}
-                onChange={(e) => setHardNumber(parseInt(e.target.value))}
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
             />
-            <span> + 10000 = {hardSum}</span>
-
-            <h3>쉬운 계산기</h3>
-            <input
-                type="number"
-                value={easyNumber}
-                onChange={(e) => setEasyNumber(parseInt(e.target.value))}
-            />
-            <span> + 1 = {easySum}</span>
+            <hr />
+            <h2>어느 나라에 있어요?</h2>
+            <p>나라: {location.country}</p>
+            <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
         </div>
     );
 }
