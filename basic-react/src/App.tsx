1e70a619d2c8910c6ca552a94cb69853474c56d2
jungcolor <jungcolor@gmail.com>
2024-07-16 23:23:52 +0900

    use useCallback

--- basic-react/src/App.tsx
+++ basic-react/src/App.tsx
@@ -1,12 +1,13 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const App = () => {
     const [number, setNumber] = useState<number | string>(0);
+    const [toggle, setToggle] = useState(true);
 
-    const someFuntion = () => {
+    const someFuntion = useCallback(() => {
         console.log(`someFunc: number: ${number}`);
         return;
-    };
+    }, [number]);
 
     useEffect(() => {
         console.log("someFunction이 변경되었습니다.");
@@ -19,6 +20,7 @@ const App = () => {
                 value={number}
                 onChange={(e) => setNumber(e.target.value)}
             />
+            <button onClick={() => setToggle(!toggle)}>{toggle.toString()}</button>
             <br />
             <button onClick={someFuntion}>Call someFunc</button>
         </div>
